9bca0ea3bed9b30363d52ea4a40f3680b5698d5a
marianomar <marianomartinez012@gmail.com>
2022-08-07 18:38:12 -0300

    primer commit

---
+++ api/database.js
@@ -0,0 +1,64 @@
+const sqlite3 = require('sqlite3').verbose();
+const path = require('path');
+const fs = require('fs');
+
+db_name = path.join(__dirname, "database.db");
+const db = new sqlite3.Database(db_name, err =>{
+    if (err){
+        return(err);
+    }else{
+        console.log('data base conected');
+    }
+    
+});
+
+
+
+//Crea la tabla con los archivos
+//db.run('CREATE TABLE text(ID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, TEXTNAME TEXT, TEXTCONTENT TEXT)')
+
+//Dado nombre de un archivo lo inserta en la D.B
+function tabla(fileName){
+
+    fs.readFile(fileName, 'utf-8', (error, data) =>{
+        if(!error){
+            const insert = 'INSERT INTO text (TEXTNAME, TEXTCONTENT) VALUES (?,?)'
+            db.run(insert, [path.basename(fileName), data]);
+        }else{
+            console.log(error.message)
+        }
+    }); 
+};
+//lee el txt e inserta el contenido en la D.B
+function iterarFs() {
+    fs.readdir('./archivos_txt', (error, files) =>{
+        files.forEach(file => {
+            tabla(path.join(__dirname, './archivos_txt', file));
+            
+        });
+        console.log('this is fine')
+    });
+    
+};
+
+iterarFs();
+
+module.exports = db
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ api/server.js
@@ -0,0 +1,69 @@
+// Create express app
+const express = require("express")
+const app = express()
+const fs = require('fs');
+
+// Data Base Reference
+const db = require("./database.js");
+
+// Server port
+var HTTP_PORT = 3000 
+
+// Start server
+app.listen(HTTP_PORT, () => {
+    console.log("Server running on port %PORT%".replace("%PORT%",HTTP_PORT))
+});
+
+// Root endpoint
+app.get("/www.melichallenge.test/text_name=&termino=", (req, res,) => {
+    res.json({"message":"Ok"})
+});
+
+function countOccurences(string, word) {
+    return  string.split(word).length - 1;
+}
+
+
+// Endpoints
+
+//Obtiene nombre del texto y termino
+app.get("/www.melichallenge.test/text_name=:TEXTNAME&termino=:term", (req, res) => {
+    var sql = "select TEXTCONTENT from text where TEXTNAME = ?"
+    var params = [req.params.TEXTNAME]
+    var term = [req.params.term]
+    db.get(sql, params, (err,row) => {
+        if (err) {
+          res.status(400).json({"error":err.message});
+        return;
+        }
+       
+       res.json("Frecuencia: " + countOccurences(row.TEXTCONTENT, term))
+    });
+      
+});
+
+//Obtiene el termino
+app.get("/www.melichallenge.test/termino=:term", (req, res) => {
+    var sql = "SELECT * from text"
+    var params = [req.params.TEXTCONTENT]
+    var term = [req.params.term]
+    db.all(sql, params, (err,row) => {
+        if (err) {
+          res.status(400).json({"error":err.message});
+        return;
+        }
+        
+        res.json("Frecuencia: " + countOccurences(row[0].TEXTCONTENT, term))
+        
+        
+    });
+      
+});
+
+
+
+// Default response for any other request
+app.use(function(req, res){
+    res.status(404);
+});
+
