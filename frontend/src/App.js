5b1a3bcc65c15ff56237db9e5c9b83458613e583
marianomar <marianomartinez012@gmail.com>
2022-08-08 23:47:23 -0300

    fetchcommit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
 import NavBar from './components/NavBar'
 import Form from './components/Form';
-    
+
+
 function App() {
   return (
     <div className="App">
--- frontend/src/components/Form.js
+++ frontend/src/components/Form.js
@@ -1,26 +1,70 @@
 import React, {useState} from "react";
-
-const Form = ({newData}) =>  {
+import '../App.css';
+const Form = () =>  {
+    const[apiRes, setApiRes] = useState("")
     const [info, setInfo] = useState("");
+    
+    
+    //obtiene el termino a buscar
     const onSubmit = (e) => {
         e.preventDefault();
         console.log(info)
+        getApiInfo(info)
 
-    } 
+    }
+    
+    // obtiene y pasa la info de la api
+    const getApiInfo = async(text) =>{
+        
+        let urlTerm = "http://localhost:3001/termino="
+        let UrlTextTerm = "http://localhost:3001/text_name=:TEXTNAME&termino=:term" 
+        
+        urlTerm = urlTerm + text
+        
+
+        //fetch
+        await fetch(urlTerm).then((apiData) =>{
+            if(!apiData.ok) throw{apiData}
+                console.log(apiData)
+                return apiData.json()
+            }).then((frecuenciaData) =>{
+                //dato de la API almacenado
+
+                setApiRes(frecuenciaData)
+                console.log(frecuenciaData)
+                
+            }).catch(error=>{
+                console.log(error)
+            })
+    
+        
+        
+
+    }
     return(
+        
         <div className="container">
 
             <form onSubmit={onSubmit}>
                 <div className="input-group mb3 mx-auto">
                     <div className="mx-auto">
                     <input type="text" className="form-control" placeholder="buscar frecuencia" onChange={(e) => setInfo(e.target.value)}/>
-                    <button className="btn btn-primary input-group-text" type="submit">Buscar termino</button>
+                    <button id="term" className="btn btn-primary input-group-text" type="submit">Buscar termino</button>
+                    <br/>
+                    <br/>
+                    <div >
+                        <p className="text-center" id="frecuencia"  >{apiRes}</p>
                     </div>
                     <br/>
-                    <div className="mx-auto">
-                    <input type="text" className="form-control" placeholder="buscar frecuencia" onChange={(e) => setInfo(e.target.value)}/>
-                    <button className="btn btn-primary input-group-text" type="submit">Buscar texto y termino</button>
                     </div>
+                    <div className="mx-auto">
+                    <input disabled id="textName" type="text" className="form-control" placeholder="buscar frecuencia" onChange={(e) => setInfo(e.target.value)}/>
+                    <button disabled className="btn btn-primary input-group-text" type="submit">Buscar texto y termino</button>
+                    </div>
+                    <br />
+                    <br />
+                    
+                   
                     
                 </div>
             </form>
